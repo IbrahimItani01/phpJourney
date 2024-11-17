82808481376421c31eec073081e0e44170d78b90
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 17:47:36 +0200

    php<>implemented the while loop to compare elements of sliced arrays and push to main array

--- classes/mergeSort.php
+++ classes/mergeSort.php
@@ -7,7 +7,27 @@ function merge ($array,$left,$middle,$right){
     $leftArray = array_slice($array,$left,$lArrayLength);
     $rightArray = array_slice($array,$middle+1,$rArrayLength);
 
+    // indices to merge the arrays back to the main one
+    $i = 0;
+    $j = 0;
+    $k = $left;
     
+    while($i < $lArrayLength && $j<$rArrayLength){
+        // comparing elements on both arrays 
+        if($leftArray[$i]<=$rightArray[$j]){
+            // in case the left array element is smaller set the value to the original array
+            $array[$k] = $leftArray[$i];
+            $i++;
+        }
+        else{
+            // in case the right array element is smaller set the value to the original array
+            $array[$k] = $rightArray[$j];
+            $j++;
+        }
+        // increment the index at main array
+        $k++;
+    }
+
 }
 
 function mergeSort ($array,$left,$right){
