cbfff92cd250b3434ce3f7dd7028e5d53150e4eb
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 21:35:08 +0200

    php<>handled the default value of optional fetch body payload

--- apis/testUser.php
+++ apis/testUser.php
@@ -6,9 +6,9 @@ $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 // boolean to check if the fetch is to copy or create new user instance
-$copy = $_POST["copy"];
+$copy = $_POST["copy"]??false;
 // in case of copy_with
-$newName = $_POST["newName"];
-$newEmail = $_POST["newEmail"];
-$newPassword = $_POST["newPassword"];
+$newName = $_POST["newName"]??"";
+$newEmail = $_POST["newEmail"]??"";
+$newPassword = $_POST["newPassword"]??"";
 
