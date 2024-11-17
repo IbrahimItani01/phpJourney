5dbe4fa0621903870e0a9189285628c9b8506df0
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 21:33:20 +0200

    php<>fetched a boolean o check if the fetch is to copy from a user instance or new user instance

--- apis/testUser.php
+++ apis/testUser.php
@@ -5,3 +5,5 @@ include "../classes/user.php";
 $name = $_POST["name"];
 $password = $_POST["password"];
 $email = $_POST["email"];
+// boolean to check if the fetch is to copy or create new user instance
+$copy = $_POST["copy"];
