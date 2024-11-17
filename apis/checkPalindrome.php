2bdf891798459eead609b0fa06cb8bfbc61fea13
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 14:09:44 +0200

    php<>pushed each character in input string into reversed array

--- apis/checkPalindrome.php
+++ apis/checkPalindrome.php
@@ -4,6 +4,6 @@ $input = $_POST["input"];
 function checkPalindrome($input) {
     $reversed = [];
     for ($i = strlen($input)-1; $i >= 0; $i--){
-
+        array_push($reversed,$input[$i]);
     }
 }
