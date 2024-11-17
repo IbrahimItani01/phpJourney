8b45053bf26bcf214d2650ee0143c520f279d71a
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 14:11:29 +0200

    big O <> added big O notation as documentation

--- apis/checkPalindrome.php
+++ apis/checkPalindrome.php
@@ -1,8 +1,9 @@
 <?php
 $input = $_POST["input"];
-
+// O(N), N being the length of the input string
 function checkPalindrome($input) {
     $reversed = [];
+    // O(N), N being the length of the input string
     for ($i = strlen($input)-1; $i >= 0; $i--){
         array_push($reversed,$input[$i]);
     }
@@ -14,6 +15,7 @@ function checkPalindrome($input) {
         return false;
     }
 }
+
 if(checkPalindrome($input)){
     http_response_code(200);
     $response = [
