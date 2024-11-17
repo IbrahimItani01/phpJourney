013a6437e5188ab6dc5fdc543a829a0bd107efb5
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 21:02:08 +0200

    php<>implemented the logic of unique id generation for each user instance

--- classes/user.php
+++ classes/user.php
@@ -4,12 +4,16 @@ class User {
     public $email;
     public $password;
     public $name;
-    public $userId;
+    public $id;
 
 
     public function __construct($email, $password,$name) {
+        $this->id = $this->generateId();
         $this->email = $email;
         $this->password = $password;
         $this->name = $name;
+    }
+    private function generateId() {
+        return uniqid('user_',true);
     }
 }
