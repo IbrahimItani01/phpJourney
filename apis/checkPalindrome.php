d177af707ef501bb5cbb3039250b32a494e79568
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 14:10:33 +0200

    php<>turned array to a string using implode method and returned true is string is equal to string

--- apis/checkPalindrome.php
+++ apis/checkPalindrome.php
@@ -6,4 +6,28 @@ function checkPalindrome($input) {
     for ($i = strlen($input)-1; $i >= 0; $i--){
         array_push($reversed,$input[$i]);
     }
+    $reversedString = implode("",$reversed);
+    if($reversedString == $input){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+if(checkPalindrome($input)){
+    http_response_code(200);
+    $response = [
+        "status" => "success",
+        "message"=> "Palindrome is checked successfully",
+        "return"=> true,
+    ];
+}
+else{
+    http_response_code(404);
+    $response = [
+        "status" => "failed",
+        "message"=> "Palindrome is checked unsuccessfully",
+        "return"=> false,
+    ];
+}
+echo json_encode($response);
