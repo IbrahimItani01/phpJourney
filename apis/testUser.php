4556f6f12522295c02067894be1f67b947839cb5
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 21:34:24 +0200

    php<>expected optional  values to update user values

--- apis/testUser.php
+++ apis/testUser.php
@@ -7,3 +7,8 @@ $password = $_POST["password"];
 $email = $_POST["email"];
 // boolean to check if the fetch is to copy or create new user instance
 $copy = $_POST["copy"];
+// in case of copy_with
+$newName = $_POST["newName"];
+$newEmail = $_POST["newEmail"];
+$newPassword = $_POST["newPassword"];
+
