21ac1b9d7c69b382dbf35591f313d4746f2b825a
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 17:00:53 +0200

    php<>created two arrays as slices from the original input array

--- classes/mergeSort.php
+++ classes/mergeSort.php
@@ -1,8 +1,15 @@
 <?php
 function merge ($array,$left,$middle,$right){
+    $lArrayLength = $middle - $left +1;
+    $rArrayLength = $right - $middle;
 
+    // temporary arrays
+    $leftArray = array_slice($array,$left,$lArrayLength);
+    $rightArray = array_slice($array,$middle+1,$rArrayLength);
+
+    
 }
 
 function mergeSort ($array,$left,$right){
 
-}
+} 
