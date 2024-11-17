fb2f8cd0cedbab0c55de4e662f08f2d2245e4156
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-17 18:15:48 +0200

    php<>implemented api logic and echo sorted array

--- apis/sortArray.php
+++ apis/sortArray.php
@@ -1,4 +1,11 @@
 <?php   
-$array = $_POST("array");
 include "../classes/mergeSort.php";
-
+$array = json_decode($_POST["array"]);
+$lastIndex = count($array)-1;
+mergeSort($array,0, $lastIndex);
+$response = [
+    "message"=>"Array sorted successfully",
+    "sortedArray"=>$array,
+];
+http_response_code(200);
+echo json_encode($response);
